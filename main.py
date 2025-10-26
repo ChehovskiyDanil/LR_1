62bd79d6dd12d24865626b824940dcccc84a3f77
chehpvskiy01@bk.ru <chehovskiy01@bk.ru>
2025-10-26 19:51:05 +0300

    The first formula has been added

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ a = float(input('Введите a = '))
 b = float(input('Введите b = '))
 c = float(input('Введите c = '))
 
-print('a = ', a)
-print('b = ', b)
-print('c = ', c)
+z1 = ((a-b-c)/2)*a
+print (('z1 = ')+str(z1))
+
