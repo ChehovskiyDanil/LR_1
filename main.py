32e87e6f22fed313e1ad7a6f1d107eb2fc1753d1
chehpvskiy01@bk.ru <chehovskiy01@bk.ru>
2025-10-26 20:10:25 +0300

    Added third formula

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ print('Ввдедите данные для вычисления по форму
 a = float(input('Введите a = '))
 b = float(input('Введите b = '))
 c = float(input('Введите c = '))
+pi = 3.14159
 
 z1 = ((a-b-c)/2)*a
 print (('z1 = ')+str(z1))
 z2 = (c-b)/a+2*b
 print (('z2 = ')+str(z2))
+z3 = (a*c*b)/pi
+print (('z3 = ')+str(z3))
