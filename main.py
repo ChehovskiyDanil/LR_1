38cd34cae8a1b01b28c9607da5bef6afb944d643
chehpvskiy01@bk.ru <chehovskiy01@bk.ru>
2025-10-26 20:14:35 +0300

    The last formula has been added

--- main.py
+++ main.py
@@ -14,3 +14,5 @@ z3 = (a*c*b)/pi
 print (('z3 = ')+str(z3))
 z4 = ((((1/a-1/(b+c))/(1/a+1/(b+c)))*(1+(pow(b,2)+pow(c,2)+pow(a,2))/(2*b*c))/(a-b-c/(a*b*c))))
 print (('z4 = ')+str(z4))
+z5 = (z1+z2+z3+z4)/4
+print (('z5 = ')+str(z5))
